25c079e686ba3ee24be1db91dc70963ce5a2849e
aivostudio <aivostudioapp@gmail.com>
2025-12-21 21:47:03 +0300

    Update env-check.js

--- api/env-check.js
+++ api/env-check.js
@@ -1,12 +1,6 @@
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   res.status(200).json({
     ok: true,
-    hasStripeSecretKey: !!process.env.STRIPE_SECRET_KEY,
-    keyPrefix: process.env.STRIPE_SECRET_KEY
-      ? process.env.STRIPE_SECRET_KEY.slice(0, 8)
-      : null,
-    keyLength: process.env.STRIPE_SECRET_KEY
-      ? process.env.STRIPE_SECRET_KEY.length
-      : 0
+    hasStripeSecretKey: Boolean(process.env.STRIPE_SECRET_KEY)
   });
-};
+}
