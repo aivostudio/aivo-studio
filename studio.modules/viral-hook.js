773a6d5755cf8ff614a59cb49dcc7bf41af07ebd
aivostudio <aivostudioapp@gmail.com>
2026-01-04 19:51:09 +0300

    Update viral-hook.js

--- studio.modules/viral-hook.js
+++ studio.modules/viral-hook.js
@@ -87,3 +87,9 @@
     }, 1500);
   });
 })();
+<!-- 🔌 ÜRÜN MODÜLLERİ -->
+<script src="/studio.modules/viral-hook.js?v=1"></script>
+<script src="/studio.modules/socialpack.js?v=1"></script>
+
+<!-- LEGACY (en son, dokunmuyoruz) -->
+<script src="/studio.js?v=999"></script>
