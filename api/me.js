45db1f54918f32b820f276a70d352f3f9c31de33
aivostudio <aivostudioapp@gmail.com>
2026-01-27 02:17:32 +0300

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,2 +1,7 @@
-// /api/me.js
-module.exports = require("./auth/me");
+return res.json({
+  ok: true,
+  sub: user.id || user.email, // hangisi senin GERÇEK user ID’inse
+  email: user.email,
+  role: user.role,
+  verified: true,
+});
