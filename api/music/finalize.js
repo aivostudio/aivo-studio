f91bd0c9f16a420745a3ba54c7a09e5d388cc84e
aivostudio <aivostudioapp@gmail.com>
2026-02-07 00:27:58 +0300

    Update finalize.js

--- api/music/finalize.js
+++ api/music/finalize.js
@@ -7,7 +7,8 @@ function getR2() {
   if (r2) return r2;
   r2 = new S3Client({
     region: "auto",
-    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    endpoint: process.env.R2_ENDPOINT,
+
     credentials: {
       accessKeyId: process.env.R2_ACCESS_KEY_ID,
       secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
