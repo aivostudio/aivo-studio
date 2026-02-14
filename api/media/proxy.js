a15459ce3a5fda6cd2e269e487ed7e36dd2156b9
aivostudio <aivostudioapp@gmail.com>
2026-02-14 23:03:40 +0300

    Update proxy.js

--- api/media/proxy.js
+++ api/media/proxy.js
@@ -16,7 +16,9 @@ export default async function handler(req, res) {
 
     // allow only known hosts (security)
     const allowedHosts = new Set([
-    "d2fnzrnvs0bpmza.cloudfront.net",
+      // eski kayıtlar + yeni kayıtlar (ikisi de kabul)
+      "dnznrvs05pmza.cloudfront.net",
+      "d2fnzrnvs0bpmza.cloudfront.net",
 
       "storage.googleapis.com",
       "cdn.runwayml.com",
