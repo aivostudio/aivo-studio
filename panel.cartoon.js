4b1a3d399718854fe79b3679ee7cdc1baf46f7e3
aivostudio <aivostudioapp@gmail.com>
2026-03-13 04:19:30 +0300

    Update panel.cartoon.js

--- panel.cartoon.js
+++ panel.cartoon.js
@@ -329,6 +329,7 @@ const elStatus = null;
         });
 
         render(merged);
+        function render(items) {
 
         function hasProcessing(items) {
           return (items || []).some((j) => {
