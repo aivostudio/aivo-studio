3859ce79886aec09367c1aad6e0acc1ced378a3a
aivostudio <aivostudioapp@gmail.com>
2026-03-18 22:55:05 +0300

    Update photofx.module.js

--- studio.modules/photofx.module.js
+++ studio.modules/photofx.module.js
@@ -1,1 +1,10 @@
+(function () {
+  if (window.__AIVO_PHOTOFX_MODULE__) return;
+  window.__AIVO_PHOTOFX_MODULE__ = true;
 
+  window.AIVOPhotoFXModule = {
+    init() {
+      console.log("[AIVO][PhotoFX] module ready");
+    }
+  };
+})();
