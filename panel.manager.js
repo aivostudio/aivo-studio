04b6abd93d2fb9ba8268762b413b77d5cd80c8ec
aivostudio <aivostudioapp@gmail.com>
2026-03-23 00:33:08 +0300

    Update panel.manager.js

--- panel.manager.js
+++ panel.manager.js
@@ -197,7 +197,10 @@
       }
 
       if(registry.has(key)){
-        console.warn("[RightPanel] duplicate register for key:", key);
+      console.warn("[RightPanel] duplicate register for key:", key, {
+  impl,
+  stack: new Error("[RightPanel duplicate stack]").stack
+});
       }
 
       registry.set(key, impl);
