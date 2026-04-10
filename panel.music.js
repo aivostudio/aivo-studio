b1ed1583d9e625c2efc471abd9c6c2bdb872c094
aivostudio <aivostudioapp@gmail.com>
2026-04-10 20:37:36 +0300

    Update panel.music.js

--- panel.music.js
+++ panel.music.js
@@ -1752,13 +1752,13 @@ function setMusicHostForEvents(el){
     render();
   }
 
-   function getHeader(){
+  function getHeader(){
     return {
       title: "Müziklerim",
       meta: "⚠️ Müzik dosyaları 14 gün saklanır.",
-      searchPlaceholder: "",
-      searchEnabled: false,
-      resetSearch: true
+      searchPlaceholder: "Müziklerde ara...",
+      searchEnabled: true,
+      resetSearch: false
     };
   }
 
