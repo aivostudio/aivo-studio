887e08c31425af662d624b78807fd5c4d6141518
aivostudio <aivostudioapp@gmail.com>
2026-05-12 20:16:32 +0300

    Update mobile.video.js

--- mobile/js/mobile.video.js
+++ mobile/js/mobile.video.js
@@ -837,9 +837,9 @@ mobileVideoToast("loading", "Video hazırlanıyor...");
             ? data
             : [];
 
-      mobileVideoJobs.length = 0;
+    mobileVideoJobs.length = 0;
 
-      rows.reverse().forEach(function(row){
+    rows.forEach(function(row){
         const outputs = Array.isArray(row.outputs) ? row.outputs : [];
 
         const firstVideoOutput = outputs.find(function(output){
