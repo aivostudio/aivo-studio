be84ff9d449d5b5471182d2bf0e7708ae8771ab7
aivostudio <aivostudioapp@gmail.com>
2026-05-24 23:08:23 +0300

    Update mobile.credits.play.js

--- mobile/js/mobile.credits.play.js
+++ mobile/js/mobile.credits.play.js
@@ -285,7 +285,14 @@ if (data.purchase_id) {
   } catch (err) {}
 }
 
-await startPlayBillingPurchase(pack);
+await startPlayBillingPurchase({
+  ...pack,
+  plan:
+    pack.plan === "baslangic" ? "tr.aivo.credits.25" :
+    pack.plan === "standart" ? "tr.aivo.credits.100" :
+    pack.plan === "pro" ? "tr.aivo.credits.200" :
+    "tr.aivo.credits.500"
+});
     } catch (err) {
     console.error("[AIVO play credits] checkout failed:", err);
         alert(
