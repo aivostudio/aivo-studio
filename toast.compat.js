adcfd9fa85e8226c4760a40ee61ef53ded018366
aivostudio <aivostudioapp@gmail.com>
2026-07-28 22:39:51 +0300

    Load reference upload hotfix

--- toast.compat.js
+++ toast.compat.js
@@ -76,6 +76,7 @@
     "/js/ad-film.project-sync.js?v=4",
     "/js/ad-film.seedance-options.js?v=2",
     "/js/ad-film.creative-plan.js?v=1",
+    "/js/ad-film.role-upload-fix.js?v=1",
     "/js/ad-film.narration-guide.js?v=1"
   ];
 
