74ccb9ebe782d5d1125a73955bb2044806f889e3
aivostudio <aivostudioapp@gmail.com>
2026-07-29 18:17:45 +0300

    Refresh ad film history playback assets

--- toast.compat.js
+++ toast.compat.js
@@ -92,10 +92,10 @@
     "/js/ad-film.reset-fix.js?v=2",
     "/js/ad-film.narration-guide.js?v=1",
     "/js/ad-film.seedance-engine.js?v=4",
-    "/js/ad-film.result-controls.js?v=7",
+    "/js/ad-film.result-controls.js?v=8",
     "/js/ad-film.output-workflow.js?v=2",
     "/js/ad-film.output-gallery.js?v=3",
-    "/js/ad-film.project-history.js?v=2",
+    "/js/ad-film.project-history.js?v=3",
     "/js/ad-film.output-main-delete.js?v=2",
     "/js/ad-film.output-sync.js?v=1"
   ];
