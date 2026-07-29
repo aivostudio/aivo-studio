577142aaf78f14cee7e7f92a3bf1bf1f383c24bc
aivostudio <aivostudioapp@gmail.com>
2026-07-29 13:37:42 +0300

    Load version-aware ad film deletion handler

--- toast.compat.js
+++ toast.compat.js
@@ -90,6 +90,7 @@
     "/js/ad-film.seedance-engine.js?v=4",
     "/js/ad-film.result-controls.js?v=2",
     "/js/ad-film.output-gallery.js?v=1",
+    "/js/ad-film.output-main-delete.js?v=1",
     "/js/ad-film.output-sync.js?v=1"
   ];
 
