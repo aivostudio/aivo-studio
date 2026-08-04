2abad1b6ab77cdf70814bf6806ce66d11691f2d4
aivostudio <aivostudioapp@gmail.com>
2026-08-04 16:14:55 +0300

    Place all ad film quality buttons inline (#183)
    * Place all three ad film quality buttons inline
    
    * Load compact three-column ad film quality layout

--- toast.compat.js
+++ toast.compat.js
@@ -47,7 +47,7 @@
     "/css/ad-film.simple-mode.css?v=3",
     "/css/ad-film.music-profile.css?v=5",
     "/css/ad-film.seedance-options.css?v=6",
-    "/css/ad-film.advanced-alignment.css?v=3",
+    "/css/ad-film.advanced-alignment.css?v=4",
     "/css/ad-film.creative-plan.css?v=1",
     "/css/ad-film.plan-simple.css?v=2",
     "/css/ad-film.creative-plan-tune.css?v=2",
