a43c3d549dffa3befd0a87d87ea3c9922738cd45
aivostudio <aivostudioapp@gmail.com>
2026-08-06 22:30:17 +0300

    Refresh ad film section script cache

--- include.partials.js
+++ include.partials.js
@@ -94,7 +94,7 @@
       if (already) return;
 
       var s = document.createElement("script");
-      s.src = "/js/index.adfilm.section.js?v=20260806_1";
+      s.src = "/js/index.adfilm.section.js?v=20260806_2";
       s.defer = true;
       document.head.appendChild(s);
     } catch (e) {
