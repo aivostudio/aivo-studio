b0e79b4317d38e8320bcbd36cdd2ac01e322802a
aivostudio <aivostudioapp@gmail.com>
2026-08-06 20:15:04 +0300

    Refresh index products style cache

--- include.partials.js
+++ include.partials.js
@@ -55,7 +55,7 @@
         var rowLink = document.createElement("link");
         rowLink.id = "aivoIndexProductsSingleRowCss";
         rowLink.rel = "stylesheet";
-        rowLink.href = "/index.products.single-row.css?v=20260806_5";
+        rowLink.href = "/index.products.single-row.css?v=20260806_6";
         document.head.appendChild(rowLink);
       }
 
